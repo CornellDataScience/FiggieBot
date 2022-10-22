7193f2f974191b0eda1bc527cac79cb47990e847
Connor Chen <cjc353@cornell.edu>
2022-10-22 16:09:46 -0400

    Half written last test

--- backend/test.py
+++ backend/test.py
@@ -1,10 +1,11 @@
 import unittest
 import asyncio
-from game import players, add_player, order_book, EMPTY_BID, EMPTY_OFFER, place_order
+from game import players, add_player, order_book, EMPTY_BID, EMPTY_OFFER, EMPTY_ORDER_BOOK, place_order, clear_book, cancel_order, accept_order, order_book_to_dict
 
 class UnitTests(unittest.TestCase):
     def setUp(self):
       players.clear()
+      clear_book() # book doesn't seem to be clearing
 
     def test_add_player(self):
       self.assertEqual(len(players), 0)
@@ -14,7 +15,7 @@ class UnitTests(unittest.TestCase):
 
     def test_place_order(self):
       # successful bid
-      self.assertEqual(order_book["bids"]["clubs"], EMPTY_BID)
+      self.assertEqual(order_book["bids"]["clubs"].order_id, EMPTY_BID.order_id)
       place_order("1", True, "clubs", 5)
       self.assertEqual(order_book["bids"]["clubs"].player_id, "1")
 
@@ -32,13 +33,26 @@ class UnitTests(unittest.TestCase):
       self.assertEqual(order_book["offers"]["clubs"].player_id, "1")
 
     def test_cancel_order(self):
-      print(players)
+      self.assertEqual(order_book["bids"]["clubs"].order_id, EMPTY_BID.order_id)
+      place_order("1", True, "clubs", 5)
+      self.assertEqual(order_book["bids"]["clubs"].player_id, "1")
+      cancel_order("1", True, "clubs")
+      self.assertEqual(order_book["bids"]["clubs"].order_id, EMPTY_BID.order_id)
 
     def test_accept_order(self):
-      print(players)
+      pass
+      # asyncio.run(add_player("1", None))
+      # asyncio.run(add_player("2", None))
+      # self.assertEqual(players["1"].balance, 350)
+      # self.assertEqual(players["2"].balance, 350)
+      # players["2"].hand["clubs"] += 1
+      # place_order("1", True, "clubs", 5)
+      # accept_order("2", True, "clubs")
+      # self.assertEqual(players["1"].balance, 355)
+      # self.assertEqual(players["2"].balance, 345)
+      # self.assertEqual(players["1"].hand["clubs"], 1)
+      # self.assertEqual(players["1"].hand["clubs"], 0)
+
 
 if __name__ == '__main__':
   unittest.main()
-  # unittests = UnitTests()
-  # print(aiounittest.helpers)
-  # aiounittest.async_test(unittests.test_add_player)()
