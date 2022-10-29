805741e9bea7e478ab6de9da1fb5c377d4662113
JamesZhang2 <jameszhang2019@outlook.com>
2022-10-29 16:19:41 -0400

    Change start_game to start_round
    Also comment out get_game_update because it will lock the program - should change to asynchronous later

--- src/agent/random_player.py
+++ src/agent/random_player.py
@@ -26,7 +26,7 @@ class RandomPlayer:
     async def run(self):
         async with websockets.connect(uri) as websocket:
             await controller.add_player(websocket, self.player_id)
-            await controller.start_game(websocket)
+            await controller.start_round(websocket)
             while True:
                 if (random.random() > 0.5):
                     await controller.place_bid(
@@ -42,8 +42,8 @@ class RandomPlayer:
                         suit=random.choice(constants.SUITS),
                         price=random.randint(self.offer_low, self.offer_high))
                 await asyncio.sleep(1)
-                game_state = await controller.get_game_update(websocket)
-                print(game_state)
+                # game_state = await controller.get_game_update(websocket) # TODO: Change this to asynchronous
+                # print(game_state)
 
 
 random_player = RandomPlayer(
