0fa0b29abedce1fc8095c0d67897a7ffbebfb470
Iram Liu <64221371+ili23@users.noreply.github.com>
2022-11-12 11:34:33 -0500

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,6 @@
-from pymongo import MongoClient
 import settings
 from datetime import datetime, timezone
+from pymongo import MongoClient
 
 client = MongoClient(settings.mongodb_uri)
 db = client.get_database("FiggieBotData")
@@ -8,18 +8,18 @@ db_games = db.get_collection('Games')
 db_rounds = db.get_collection('Rounds')
 db_orders = db.get_collection('Orders')
 
-def write_games(players):
+def write_games(game_id,players):
     """Add a game to games database once a game is started, perhaps write final balances of players at end game?
     """
     player_array = [str(player) for player in players]
     db_games.insert_one({
         #need to figure out what to do with the game rounds aka game id
-        #"game_id" : game_id
+        "game_id" : game_id,
         "players" : player_array
     })
     print("Successfully added the game to the database.")
 
-def write_rounds(round_id,players):
+def write_rounds(game_id, round_id,players):
     """Add a round to rounds database with starting balances?
     """
     #need to somehow get games id from database and add it to the order database
@@ -28,19 +28,19 @@ def write_rounds(round_id,players):
         balance[player] = player.balance
     db_rounds.insert_one({
         #need to figure out what to do with the game rounds aka game id
-        #"game_id" : game_id
+        "game_id" : game_id,
         "round_id" : round_id,
         "players": balance
     })
     print("Successfully added the round to the database.")
 
-def write_orders(round_id, is_bid, suit, price, buyer, seller, action):
+def write_orders(game_id, round_id, is_bid, suit, price, buyer, seller, action):
     """Add orders to orders database
     """
     db_orders.insert_one(
         {
             #need to figure out what to do with the game rounds aka game id
-            #"game_id" : game_id
+            "game_id" : game_id,
             "round_id" : round_id,
             "timestamp" : datetime.now(),
             "is_bid" : is_bid,
